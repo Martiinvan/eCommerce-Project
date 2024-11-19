2335c0f687c54fe6da5ac2ff0ba6fa23e357970a
MartiinVan <vancompernolle26@gmail.com>
2024-11-18 21:37:05 -0300

    add products

--- dashboard/js/tables/tablesNums.js
+++ dashboard/js/tables/tablesNums.js
@@ -1,3 +1,7 @@
+
+// FUNCIONES PARA FETCH
+
+// USUARIOS
 function users() {
     return fetch('https://673b7874339a4ce4451c54ba.mockapi.io/Usuario')
         .then(response => response.json())
@@ -11,11 +15,30 @@ function users() {
             return 0; // Retorna 0 en caso de error
         });
 }
+// PRODUCTOS
+function product() {
+    return fetch('https://673bca8896b8dcd5f3f77b33.mockapi.io/Producto')
+        .then(response => response.json())
+        .then(data => {
+            const cantidadProductos = Object.keys(data).length;
+            console.log('Cantidad de Productos:', cantidadProductos);
+            return cantidadProductos;
+        })
+        .catch(error => {
+            console.log('Error:', error);
+            return 0; // Retorna 0 en caso de error
+        });
+}
 
-// LLAMAMOS AL SPAN
+
+
+// LLAMAMOS A LOS SPAN
 const numUsu = document.querySelector('.num-usu');
-
-// MODIFICAMOS EL CONTENIDO DEL SPAN DE FORMA ASINCRÓNICA
+const numProd = document.querySelector('.num-prod')
+// MODIFICAMOS EL CONTENIDO DEL SPAN
 users().then(cantidad => {
     numUsu.textContent = cantidad;
 });
+product().then(cantidad => {
+    numProd.textContent = cantidad;
+})
