c5117e626b05a01c673980cfff107c7e25305392
AgusC0 <colinaagustin92@gmail.com>
2024-11-25 00:24:26 -0300

    Home Modificado y los valores se actualizan

--- dashboard/js/tables/tablesNums.js
+++ dashboard/js/tables/tablesNums.js
@@ -30,15 +30,52 @@ function product() {
         });
 }
 
+//Ventas
+function ventas() {
+    return fetch('https://6742ade3b7464b1c2a626fd7.mockapi.io/Venta')
+        .then(response => response.json())
+        .then(data => {
+            const cantidadventas = Object.keys(data).length;
+            console.log('Cantidad de Ventas:', cantidadventas);
+            return cantidadventas;
+        })
+        .catch(error => {
+            console.log('Error:', error);
+            return 0; // Retorna 0 en caso de error
+        });
+}
+//Categoria
+function categorias() {
+    return fetch('https://673b7874339a4ce4451c54ba.mockapi.io/Categoria')
+        .then(response => response.json())
+        .then(data => {
+            const cantidadcategorias = Object.keys(data).length;
+            console.log('Cantidad de Categorias:', cantidadcategorias);
+            return cantidadcategorias;
+        })
+        .catch(error => {
+            console.log('Error:', error);
+            return 0; // Retorna 0 en caso de error
+        });
+}
 
 
 // LLAMAMOS A LOS SPAN
+const numVentas = document.querySelector('.num-ven'); 
 const numUsu = document.querySelector('.num-usu');
 const numProd = document.querySelector('.num-prod')
+const numcat = document.querySelector('.num-prodS')
+
 // MODIFICAMOS EL CONTENIDO DEL SPAN
 users().then(cantidad => {
     numUsu.textContent = cantidad;
 });
 product().then(cantidad => {
     numProd.textContent = cantidad;
+})
+ventas().then(cantidad => {
+    numVentas.textContent = cantidad;
+})
+categorias().then(cantidad => {
+    numcat.textContent = cantidad;
 })
